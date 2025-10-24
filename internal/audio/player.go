0549d55fbcb8cf01035fd4dbea1b80a307643718
Victor <vbivol394@gmail.com>
2025-10-24 11:41:32 +0300

    am folosit modul oto pentru a putea reda cantecul

---
+++ internal/audio/player.go
@@ -0,0 +1,38 @@
+package player
+
+import (
+	"bytes"
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/go-mp3"
+	"os"
+	"time"
+)
+
+func PlayFile(path, action string) {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		panic("reading" + path + "failed" + err.Error())
+	}
+
+	fileReader := bytes.NewReader(file)
+	decodedMp3, err := mp3.NewDecoder(fileReader)
+
+	if err != nil {
+		panic("mp3 new decoder failed" + err.Error())
+	}
+
+	op := &oto.NewContextOptions{}
+	op.SampleRate = 44100
+	op.ChannelCount = 2
+	op.Format = oto.FormatSignedInt16LE
+	otoCtx, readyChan, err := oto.NewContext(op)
+	<-readyChan
+	player := otoCtx.NewPlayer(decodedMp3)
+
+	if action == "play" {
+		player.Play()
+	}
+	for player.IsPlaying() {
+		time.Sleep(time.Millisecond)
+	}
+}
