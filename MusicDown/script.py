e2b9ac4b324839de7046b1b05edde071347c7903
Victor <vbivol394@gmail.com>
2025-11-22 16:45:46 +0200

    am facut un script python pentru descarcare playlisturilor

---
+++ MusicDown/script.py
@@ -0,0 +1,26 @@
+import subprocess
+
+done = True
+link = ''
+while done:
+    link = str(input('Introdu un link PlayList sau link normal : '))
+    if link and len(link) >15:
+        done = False
+    else:
+        print('Link gresit incerca dinou ')
+
+template = str(input('Introdu un nume Ditrectoriu (sau lasa gol ca default) : '))
+if len(template) == 0:
+    template =' %(playlist)s/%(playlist_index)s - %(title)s.%(ext)s'
+
+command = [
+    "yt-dlp",
+    "-o", template,
+    "--write-thumbnail",
+    "-f", "bestaudio",
+    link
+]
+
+
+subprocess.run(command)
+
