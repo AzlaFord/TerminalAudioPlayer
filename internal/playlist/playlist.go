d57c4504d1fdc77ccebfaff4c6ab4f452fcb3ce8
Victor <vbivol394@gmail.com>
2025-12-01 12:15:51 +0200

    am facut sa geseasca tot ce e in directoriul == playlistul si sal returneze

--- internal/playlist/playlist.go
+++ internal/playlist/playlist.go
@@ -5,10 +5,14 @@ import (
 	"os"
 )
 
+// Deci lista de cantece in Playlist
+
 type Track struct {
 	Path  string
 	Title string
 }
+
+// Lista de Playlisturi in Home/User/Music fiecare fisier in Music e socotit ca Playlist
 
 type Playlist struct {
 	Name   string
@@ -26,12 +30,34 @@ func DiscoverPaylists() ([]Playlist, error) {
 	}
 
 	for _, file := range files {
+		if !file.IsDir() {
+			continue
+		}
+
+		var pathPlaylist = "/home/bivol/Music/" + file.Name()
+		var tracksInThisPlayList []Track
+
+		track, err := os.ReadDir(pathPlaylist)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, trackData := range track {
+			tracks := Track{
+				Path:  pathPlaylist + "/" + trackData.Name(),
+				Title: trackData.Name(),
+			}
+			tracksInThisPlayList = append(tracksInThisPlayList, tracks)
+		}
+
 		playList := Playlist{
 			Name:   file.Name(),
-			Tracks: []Track{},
+			Tracks: tracksInThisPlayList,
 		}
+
 		playLists = append(playLists, playList)
+
 	}
+
 	return playLists, nil
 
 }
