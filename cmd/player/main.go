24965729ae79bee417eb3f1f848893e3eb3f0076
Victor <vbivol394@gmail.com>
2025-12-02 11:43:53 +0200

    modificat main go nu mai e nevoie de nagiarea aia :

--- cmd/player/main.go
+++ cmd/player/main.go
@@ -1,53 +1,8 @@
 package main
 
-import (
-	"TerminalAudioPlayer/internal/audio"
-	"TerminalAudioPlayer/internal/playlist"
-	"fmt"
-)
+import "TerminalAudioPlayer/internal/audio"
 
 func main() {
 
-	var index int
-	var index2 int
-	var cmd string
-
-	playlists, err := playlist.DiscoverPlaylists()
-	if err != nil {
-		fmt.Println("erroare", err)
-	}
 	audio.Init()
-	for {
-		fmt.Scanln(&cmd)
-		selectedPlaylists := playlists[index]
-
-		switch cmd {
-
-		case "k":
-			for i, playlist := range playlists {
-				fmt.Println(i, playlist.Name)
-			}
-		case "l":
-			for i, song := range selectedPlaylists.Tracks {
-				fmt.Println(i, song.Title)
-			}
-		case "s":
-			fmt.Println("Introdu index playlist")
-			fmt.Scanln(&index)
-
-		case "t":
-			fmt.Println("Introdu index cantec")
-			fmt.Scanln(&index2)
-		case "p":
-			selectSong := selectedPlaylists.Tracks[index2]
-			err = audio.PlayFile(selectSong.Path)
-			if err != nil {
-				fmt.Println("Eroare la redare", err)
-			}
-		}
-
-		if cmd == "q" {
-			break
-		}
-	}
 }
