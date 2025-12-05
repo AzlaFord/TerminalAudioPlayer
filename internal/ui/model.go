894e079f34853a0999606f0310f0153ab205c54e
Victor <vbivol394@gmail.com>
2025-12-05 12:42:23 +0200

    am schimbat look la lista :

--- internal/ui/model.go
+++ internal/ui/model.go
@@ -3,16 +3,32 @@ package ui
 import (
 	"TerminalAudioPlayer/internal/playlist"
 	"fmt"
+	"io"
 	"strconv"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+const listHeight = 14
 
 type item struct {
 	title, desc string
 	index       int
 }
+
+type itemDelegate struct{}
 
 func (i item) Title() string {
 	return i.title
@@ -41,6 +57,7 @@ type Model struct {
 
 	status          string
 	focusOnPlaylist bool
+	choice          string
 }
 
 func NewModel() (Model, error) {
@@ -53,17 +70,24 @@ func NewModel() (Model, error) {
 	}
 	// incarca playlisturile in playlistItem
 
-	for idx, pl := range listPl {
+	for _, pl := range listPl {
 		length := "Tracks " + strconv.Itoa(len(pl.Tracks))
-		items = append(items, item{title: pl.Name, desc: length, index: idx})
+		items = append(items, item{title: pl.Name, desc: length})
 	}
 
 	// incarca playlisturile in trackList
 	for _, pl := range tracks {
 		fmt.Println(pl.Title)
 	}
-	l := list.New(items, list.NewDefaultDelegate(), 0, 0)
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
 	l.Title = "Playlists"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
 
 	if len(listPl) > 0 {
 		tracks = listPl[0].Tracks
@@ -76,4 +100,25 @@ func NewModel() (Model, error) {
 		focusOnPlaylist: true,
 		playListItem:    l,
 	}, nil
+}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s %s ", index+1, i.title, i.desc)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
 }
